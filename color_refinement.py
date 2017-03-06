2586b67b264afb524eba59b21c57cc07820c2d4e
fluffy-unicorn <matthijsvhelden@gmail.com>
2017-03-06 13:39:56 +0100

    update

--- color_refinement.py
+++ color_refinement.py
@@ -1,68 +1,85 @@
 from graph_io import load_graph, write_dot
 
+# Print a dot file with filename and graph
 def print_dot(filename, G):
 	with open(filename, 'w') as f:
 		write_dot(G, f)
 
-def have_same_neighbours(v1, v2):
-	l1 = []
-	l2 = []
-	for n in v1.neighbours:
-		l1.append(n.colornum)
-	for n in v2.neighbours:
-		l2.append(n.colornum)
-	l1.sort()
-	l2.sort()
-	return l1 == l2
+def get_neighbourhood(v): 				### Optimisation: store neighbourhood color list in vertex class ???
+	result = []
+	for n in v.neighbours:
+		result.append(n.colornum)
+	result.sort()
+	return result
 
-def is_in_list(value, lst):
-	for l in lst:
-		if l[1] == value:
-			return True
-	return False
+def get_known_neighbourhood(n, lst):
+	for i in range(len(lst)):
+		if lst[i][2] == n:
+			return i
+	return -1
+
+def get_index(v, lst):
+	for i in range(len(lst)):
+		if lst[i][1] == v:
+			return i
+	return -1
 
 # Load graph from file
-with open('simplest.grl') as f:
+with open('colorref_smallexample_6_15.grl') as f:
 	L=load_graph(f, read_list=True)
 
 # Rename labels
 for v in L[0][0].vertices:
-	v.label = "0: " + str(v.label)
+	v.label = "(0: " + str(v.label) + ") "
 for v in L[0][1].vertices:
-	v.label = "1: " + str(v.label)
+	v.label = "(1: " + str(v.label) + ") "
+
+vertex_count = len(L[0][0])
 # Create a disjoint union of the first two graphs
 G = L[0][0] + L[0][1]
 
 partitions = {}
-last_color = 0
+last_colour = 0
 # Initially color the vertices based on degree
 for v in G.vertices:
 	v.colornum = v.degree
-	if last_color < v.degree:
-		last_color = v.degree
+	if last_colour < v.degree:
+		last_colour = v.degree
 	if v.degree in partitions:
 		partitions[v.degree].append(v)
 	else:
 		partitions[v.degree] = [v]
+print(partitions)
+print_dot('input.dot', G)
 
-last_color += 1
-index = 0
+last_colour += 1
 # Number of loops equals number of vertices
-for v in range(1):
+for v in range(vertex_count):
+	print("Run " + str(v+1) + "/" + str(vertex_count))
 	for p in partitions.copy():
 		part = partitions[p]
 		n = len(part)
-		tmp = []
+		tmp = [] # list of (colour, vertex, neighbourhood)-tuples
 		for i in range(n):
 			for j in range(i+1, n):
-				if not have_same_neighbours(part[i], part[j]):
-					if is_in_list(part[i], tmp):
-						tmp.append((part[i].colornum, part[j]))
+				ni = get_neighbourhood(part[i])
+				nj = get_neighbourhood(part[j])
+				if not ni == nj:
+					colour = get_known_neighbourhood(nj, tmp)
+					idx = get_index(part[j], tmp)
+					if idx == -1:
+						if colour >= 0:
+							tmp.append((colour, part[j], nj))
+						else:
+							tmp.append((last_colour, part[j], nj))
+							last_colour += 1
 					else:
-						tmp.append((last_color, part[j]))
-					last_color += 1
+						if colour >= 0:
+							tmp[idx] = (colour, part[j], nj)
+						else:
+							tmp[idx] = (last_colour, part[j], nj)
+							last_colour += 1
 		# Update the dictionary and the vertices
-		print(tmp)
 		for t in tmp:
 			if t[1] in part:
 				part.remove(t[1])
@@ -71,10 +88,7 @@ for v in range(1):
 			else:
 				partitions[t[0]] = [t[1]]
 			t[1].colornum = t[0]
-		index += 1
-				
-				
-			
+		
 # Write output to .dot files
 #for (i, H) in enumerate(L[0]):
 #	print_dot('output' + str(i) + '.dot', H)
