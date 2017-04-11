53df3d6b28c8b451103b31c57e37d866dcd7899a
fluffy-unicorn <matthijsvhelden@gmail.com>
2017-04-11 09:01:56 +0200

    A

---
+++ inleveren/testBasic.sh
@@ -0,0 +1,8 @@
+time python main.py ../Basic/basicAut1.gr 
+time python main.py ../Basic/basicAut2.gr 
+time python main.py ../Basic/basicGI1.grl -i
+time python main.py ../Basic/basicGI2.grl -i
+time python main.py ../Basic/basicGI3.grl -i
+time python main.py ../Basic/basicGIAut.grl -i
+time python main.py ../Basic/basicGIAut.grl -a
+
